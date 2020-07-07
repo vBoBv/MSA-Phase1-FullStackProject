c75c2107ec6921055d6f8fc203044e3b7341b35f
vBoBv <vannponhvath@gmail.com>
2020-07-08 01:17:19 +1200

    tested api request

--- react-app-phase1/src/components/App.tsx
+++ react-app-phase1/src/components/App.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ThemeProvider } from '@material-ui/styles';
 import Header from './Header/Header';
 import theme from './Theme/Theme';
+import zomato from './api/zomato';
 
 const App = () => {
+	const [term, setTerm] = useState('Auckland');
+
+	useEffect(() => {
+		const search = async () => {
+			const { data } = await zomato.get('/locations', {
+				params: {
+					query: term
+				}
+			});
+			console.log(data);
+		};
+		search();
+	}, [term]);
+
 	return (
 		<ThemeProvider theme={theme}>
 			<Header />
