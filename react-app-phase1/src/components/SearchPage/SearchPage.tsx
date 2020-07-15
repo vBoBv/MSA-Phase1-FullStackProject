6e482f0857dadf4d6d7e903b2ae99e2238cfdfb3
vBoBv <vannponhvath@gmail.com>
2020-07-15 22:57:23 +1200

    added result length to SearchPage

--- react-app-phase1/src/components/SearchPage/SearchPage.tsx
+++ react-app-phase1/src/components/SearchPage/SearchPage.tsx
@@ -2,12 +2,37 @@ import React, { useState, useEffect } from 'react';
 import RestaurantList from '../RestaurantList/RestaurantList';
 import { RestaurantProps } from '../../common/Interfaces';
 import ProgressBar from '../ProgressBar/ProgressBar';
+import { Typography, Paper } from '@material-ui/core';
+import { Theme, makeStyles } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
 
 interface SearchPageProps {
 	restaurantData: RestaurantProps[];
 }
 
+const useStyles = makeStyles((theme: Theme) => ({
+	resultFound: {
+		textAlign: 'center',
+		marginBottom: '1rem'
+	},
+	resultNotFound: {
+		minHeight: '75vh',
+		padding: '1rem',
+		margin: '3rem',
+		backgroundColor: '#D3D3D3',
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	searchIcon: {
+		fontSize: '5rem',
+		fill: '#A9A9A9'
+	}
+}));
+
 const SearchPage = ({ restaurantData }: SearchPageProps) => {
+	const classes = useStyles();
 	const [hidden, setHidden] = useState(true);
 
 	useEffect(() => {
@@ -15,6 +40,23 @@ const SearchPage = ({ restaurantData }: SearchPageProps) => {
 			setHidden(false);
 		}, 3500);
 	}, []);
+
+	const renderDataLength = () => {
+		if (restaurantData.length > 0) {
+			return (
+				<div className={classes.resultFound}>
+					<Typography variant='caption'>There are {restaurantData.length} results match your search term.</Typography>
+				</div>
+			);
+		} else {
+			return (
+				<Paper className={classes.resultNotFound} elevation={3}>
+					<SearchIcon className={classes.searchIcon} />
+					<Typography>Sorry, No Result Found</Typography>
+				</Paper>
+			);
+		}
+	};
 
 	const renderRestaurantList = restaurantData.map((item) => {
 		return (
@@ -24,7 +66,18 @@ const SearchPage = ({ restaurantData }: SearchPageProps) => {
 		);
 	});
 
-	return <div>{hidden ? <ProgressBar /> : renderRestaurantList}</div>;
+	return (
+		<div>
+			{hidden ? (
+				<ProgressBar />
+			) : (
+				<React.Fragment>
+					{renderDataLength()}
+					{renderRestaurantList}
+				</React.Fragment>
+			)}
+		</div>
+	);
 };
 
 export default SearchPage;
