47cf1f046b43ee65d7475dd0abe8166a8ae7f084
vBoBv <vannponhvath@gmail.com>
2020-07-19 22:53:21 +1200

    changed color palette

--- react-app-phase1/src/components/Theme/Theme.tsx
+++ react-app-phase1/src/components/Theme/Theme.tsx
@@ -1,7 +1,7 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
 const lightGrey = '#eef2f3';
-const softOrange = '#FFA17F';
+const softOrange = '#4ca2cd';
 
 export default createMuiTheme({
 	palette: {
